f0106c20cc2a226a5c1d3e369a1417d054fe1fbd
Meng-emulab <mengwanguc@gmail.com>
2020-04-26 08:24:52 -0600

    build.sh

--- cpp-driver-2.12.0/build.sh
+++ cpp-driver-2.12.0/build.sh
@@ -3,3 +3,6 @@ cd build
 cmake ..
 make -j 32
 make install -j 32
+
+ln -s /usr/local/lib/x86_64-linux-gnu/libcassandra.so.2 /usr/lib64/libcassandra.so.2
+ln -s /usr/local/lib/x86_64-linux-gnu/libcassandra.so.2 /usr/lib/libcassandra.so.2
