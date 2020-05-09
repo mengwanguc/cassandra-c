47433fe8a59131f283b222bd4da2a46d7d27621a
Meng Wang github desktop <mengwanguc@gmail.com>
2020-05-09 11:04:24 -0500

    check next_host

--- cpp-driver-2.14.0/src/request_handler.cpp
+++ cpp-driver-2.14.0/src/request_handler.cpp
@@ -199,9 +199,19 @@ void RequestHandler::init(const ExecutionProfile& profile, ConnectionPoolManager
 void RequestHandler::execute() {
   RequestExecution::Ptr request_execution(new RequestExecution(this));
   running_executions_++;
-//  std::cout << "RequestHandler::execute for host"
-//		  << request_execution->current_host()->address_string()
-//		  << std::endl;
+  std::cout << "RequestHandler::execute for host"
+		  << request_execution->current_host()->address_string()
+		  << std::endl;
+  std::cout << "RequestHandler::next host"
+		  << next_host(RequestHandler::Protected())->address_string()
+		  << std::endl;
+  std::cout << "RequestHandler::next host"
+ 		  << next_host(RequestHandler::Protected())->address_string()
+ 		  << std::endl;
+  std::cout << "RequestHandler::next host"
+ 		  << next_host(RequestHandler::Protected())->address_string()
+ 		  << std::endl;
+
   internal_retry(request_execution.get());
 }
 
