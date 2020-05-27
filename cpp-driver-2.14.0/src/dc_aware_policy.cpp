31bcf011e007079e5d580e8b9b72d01d7bcfa963
Meng Wang github desktop <mengwanguc@gmail.com>
2020-05-26 22:32:38 -0500

    zipfian

--- cpp-driver-2.14.0/src/dc_aware_policy.cpp
+++ cpp-driver-2.14.0/src/dc_aware_policy.cpp
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-
+#include <time.h>
 #include "dc_aware_policy.hpp"
 
 #include "logger.hpp"
